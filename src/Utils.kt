9b37db05bf065822591df61a7bff2c938f4ab09d
Daniel Lamblin <dlamblin+github@gmail.com>
2021-12-05 18:14:00 -0500

    Add a list of ints utility method

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,16 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+val digits = Regex("""\d+""")
+
+/**
+ * Converts a list of strings to a list of lists of integers
+ */
+fun List<String>.toListOfInts(): List<List<Int>> {
+    return map { s ->
+        digits.findAll(s).map{ mr ->
+            mr.value.toInt()
+        }.toList() }
+
+}
