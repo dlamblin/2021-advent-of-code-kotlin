bf9b302071447f844b6cb1d3f2462c24fd26aa67
Daniel Lamblin <dlamblin+github@gmail.com>
2021-12-07 01:06:41 -0500

    Day 7 cleanup.

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,44 +1,51 @@
 import java.util.Collections.max
 import java.util.Collections.min
 import kotlin.math.abs
+import kotlin.system.measureNanoTime
 
 fun main() {
     val day = "Day07"
 
-    fun part1(input: List<List<Int>>): Int {
-        val min = min(input.first())
-        val max = max(input.first())
-        return min((min..max).map {
-                col -> input.first().map {
-            kotlin.math.abs(it - col)
-                }.sum() })
+    fun part1(input: List<Int>): Int {
+        val min = min(input)
+        val max = max(input)
+        return min((min..max).map { c -> input.sumOf { abs(it - c) } })
     }
 
-    fun part2(input: List<List<Int>>): Int {
-        val min = min(input.first())
-        val max = max(input.first())
-        var cost: Int = 0
-        val costMap = (0..max).associateWith { k -> cost += k; cost }
-        //costMap.forEach{ (k, v) -> println("$k -> $v") }
-        return min((min..max).map {
-                col -> input.first().sumOf { costMap[abs(it - col)] ?: 0 }
-        })
+    fun part1Alt(input: List<Int>): Int {
+        var (min, max) = Pair(input.first(), input.first())
+        input.forEach { min = min.coerceAtMost(it); max = max.coerceAtLeast(it) }
+        return min((min..max).map { c -> input.sumOf { abs(it - c) } })
+    }
 
+    fun part2(input: List<Int>): Int {
+        val min = min(input)
+        val max = max(input)
+        var cost = 0
+        val costMap = (0..max).associateWith { cost += it; cost }
+        return min((min..max).map { c -> input.sumOf { costMap[abs(it - c)] ?: 0 } })
     }
 
     // test if implementation meets criteria from the description, like:
     println("\t#\tTesting")
-    val testInput = readInput(day + "_test").toListOfInts()
+    val testInput = readInput(day + "_test").toListOfInts().first()
     println("\t##\tPart 1")
-    val testResult1 = part1(testInput)
-    println("Result:\t\t${testResult1}")
+    var testResult1 = 0
+    var timeInNanos = measureNanoTime {
+        testResult1 = part1(testInput)
+    }
+    println("Result:\t\t${testResult1} in $timeInNanos ns")
+    timeInNanos = measureNanoTime {
+        testResult1 = part1Alt(testInput)
+    }
+    println("ResultAlt:\t${testResult1} in $timeInNanos ns")
     check(testResult1 == 37)
     println("\t##\tPart 2")
     val testResult2 = part2(testInput)
     println("Result:\t\t${testResult2}")
     check(testResult2 == 168)
 
-    val input = readInput(day).toListOfInts()
+    val input = readInput(day).toListOfInts().first()
     println("\t#\tRunning")
     println("\t##\tPart 1")
     println("Result:\t\t${part1(input)}")
