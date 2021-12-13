0a784c04a751e8347b93d17c56ccc23e5f0ccd11
Daniel Lamblin <dlamblin+github@gmail.com>
2021-12-13 00:48:26 -0500

    Day 10 added stack operations to MutableList
    an unused matching up map for parens was made with zip

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,34 +1,43 @@
+// Stack methods
+typealias Stack<T> = MutableList<T>
+fun <T> Stack<T>.push(item: T) = add(item)
+fun <T> Stack<T>.pop(): T? = removeLastOrNull()
+fun <T> Stack<T>.peek(): T? = lastOrNull()
+
 fun main() {
     val day = "Day10"
     val score = mapOf(
-    ')' to 3,
-    ']' to 57,
-    '}' to 1197,
-    '>' to 25137,
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137,
     )
     val score2 = mapOf(
-    '(' to 1,
-    '[' to 2,
-    '{' to 3,
-    '<' to 4,
+        '(' to 1,
+        '[' to 2,
+        '{' to 3,
+        '<' to 4,
     )
     val open = arrayOf('(','[','{','<')
     val close = arrayOf(')',']','}','>')
+    val match = mapOf(
+        *(open zip close).toTypedArray(),
+        *(close zip open).toTypedArray())
     fun part1(input: List<String>): Int {
         var tot = 0
         for (line in input) {
-            val stack = MutableList(0){' '}
+            val stack:Stack<Char> = MutableList(0){' '}
             for (c in line) {
                 if (c in open) {
-                    stack.add(c)
+                    stack.push(c)
                 } else {
                     val (i, _) = close.withIndex()
                         .find { it.value == c }!!
-                    if (stack.last() != open[i]) {
+                    if (stack.peek() != open[i]) {
                         tot += score[c]!!
                         break
                     } else {
-                        stack.removeAt(stack.size-1)
+                        stack.pop()
                     }
                 }
             }
@@ -39,21 +48,21 @@ fun main() {
     fun part2(input: List<String>): Long {
         val scores = mutableListOf<Long>()
         for (line in input) {
-            val stack = MutableList(0){' '}
+            val stack:Stack<Char> = MutableList(0){' '}
             var tot = 0L
             var brk = false
             for (c in line) {
                 // println("$c to ${stack.joinToString("")}")
                 if (c in open) {
-                    stack.add(c)
+                    stack.push(c)
                 } else {
                     val (i, _) = close.withIndex()
                         .find { it.value == c }!!
-                    if (stack.last() != open[i]) {
+                    if (stack.peek() != open[i]) {
                         brk = true
                         break
                     } else {
-                        stack.removeAt(stack.size-1)
+                        stack.pop()
                     }
                 }
             }
@@ -61,8 +70,7 @@ fun main() {
                 print("${stack.joinToString("")} scores ")
                 while (stack.size > 0) {
                     tot *= 5
-                    tot += score2[stack.last()]!!
-                    stack.removeAt(stack.size-1)
+                    tot += score2[stack.pop()]!!
                 }
                 println(tot)
                 scores.add(tot)
